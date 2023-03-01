b514f5bc8ccb1b3d8c839d8fc1f65e360f9feb01
caregor <caregor@gmail.com>
2023-03-01 13:41:19 +0500

    [Task 3] added new method numberInInterval() and tests;

--- Test/seminars/third/hw/HomeworkTest.java
+++ Test/seminars/third/hw/HomeworkTest.java
@@ -2,6 +2,8 @@ package seminars.third.hw;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 import seminars.third.coverage.hw.Homewowrk;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -19,5 +21,15 @@ public class HomeworkTest {
     @Test
     void oddNumber(){
         assertFalse(hw.evenOddNumber(1));
+    }
+    @ParameterizedTest
+    @ValueSource(ints = {26,50,99})
+    void positiveInput (int input){
+        assertTrue(hw.numberInInterval(input));
+    }
+    @ParameterizedTest
+    @ValueSource(ints = {25,100})
+    void negativeInput (int input){
+        assertFalse(hw.numberInInterval(input));
     }
 }
--- src/seminars/third/coverage/hw/Homewowrk.java
+++ src/seminars/third/coverage/hw/Homewowrk.java
@@ -4,4 +4,7 @@ public class Homewowrk {
     public static boolean evenOddNumber(int n) {
         return n % 2 == 0;
     }
+    public static boolean numberInInterval(int input){
+        return input > 25 && input < 100;
+    }
 }
